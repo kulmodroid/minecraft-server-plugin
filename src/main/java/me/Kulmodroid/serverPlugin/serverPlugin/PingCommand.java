90adfbf3de145296a0a57d8bbeaad5e6f7a91a93
Valentin Nikotin <rednikotin@gmail.com>
2025-06-07 18:40:33 +0300

    Refactor duel system and remove blocking loops

--- src/main/java/me/Kulmodroid/serverPlugin/serverPlugin/PingCommand.java
+++ src/main/java/me/Kulmodroid/serverPlugin/serverPlugin/PingCommand.java
@@ -4,17 +4,11 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class PingCommand implements CommandExecutor
-{
+public class PingCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
-    {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         String msg = "Welcome to server " + sender.getName() + "!";
-
         sender.sendMessage(msg);
-
         return true;
     }
-
-
 }
